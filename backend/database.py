bc160a82a0f0879106c8c7d5cce4b108890111f5
Kailash Ganesh <kailash.sarvaraju22@gmail.com>
2026-05-17 11:55:19 +0530

    Fixed database connection

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:2007@localhost/skillgap"
+DATABASE_URL = "postgresql://skill_gap_analyzer_user:tHYZQRB4mTrRegXNergGlILVKuVCBtOl@dpg-d84ltn3eo5us73eem580-a.virginia-postgres.render.com/skill_gap_analyzer"
 
 engine = create_engine(DATABASE_URL)
 
