977096ec27e75acc4e4f8294e7f877d8425fe5da
Kailash Ganesh <kailash.sarvaraju22@gmail.com>
2026-05-19 11:42:02 +0530

    Connected production backend

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
- baseURL: "http://localhost:8000",
+  baseURL:
+    "https://skill-gap-analyzer-i1ep.onrender.com"
 });
 
-export default API;
+export default API; 
