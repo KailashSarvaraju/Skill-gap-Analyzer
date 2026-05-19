fbec824b3cfbad0fc9d65a46cde13545258e1f4c
Kailash Ganesh <kailash.sarvaraju22@gmail.com>
2026-05-19 11:51:21 +0530

    Updated CORS settings

--- backend/main.py
+++ backend/main.py
@@ -55,13 +55,13 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://astonishing-centaur-126f5f.netlify.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 # =========================================
 # DATABASE CONNECTION
 # =========================================
