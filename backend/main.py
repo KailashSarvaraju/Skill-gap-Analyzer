f02859c839e8a340537e073438950c697fa1df4b
Kailash Ganesh <kailash.sarvaraju22@gmail.com>
2026-05-21 11:28:36 +0530

    Fixed CORS for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -55,8 +55,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://skill-gap-analyzer-e5ol.vercel.app"
+        "https://skill-gap-analyzer-a1j1.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
